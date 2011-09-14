a10eac596e51910c2adcaee33dcb557b01330cc2
Grigory <grigory.v.p@gmail.com>
2011-09-14 23:29:24 +0400

    Added simple distutils setup

---
+++ setup.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from distutils.core import setup
+
+setup(
+  name='sigma',
+  version='0.1.0',
+  description='Language neutral source code files preprocessor.',
+  author='Grigory Petrov',
+  author_email='grigory.v.p@gmail.com',
+  url='http://github.com/eyeofhell/sigma/',
+  packages=[ 'sigma' ],
+  classifiers=[
+    'Development Status :: 1 - Prototype',
+    'Environment :: Console',
+    'Intended Audience :: Developers',
+    'License :: GPLv3',
+    'Operating System :: OS Independent',
+    'Programming Language :: Python',
+    'Topic :: Utilities' ])
+
