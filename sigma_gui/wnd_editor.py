450d5fe394dfdf6c74198ee25f0de428b7a2befb
Grigory Petrov <grigory.v.p@gmail.com>
2012-06-27 00:13:13 +0400

    Editor window now has editor control and size grip.

--- sigma_gui/wnd_editor.py
+++ sigma_gui/wnd_editor.py
@@ -8,6 +8,11 @@ class WndEditor( pu.Wnd ) :
 
   def __init__( self ) :
     super( WndEditor, self ).__init__()
+    with pu.Rack( self ) :
+      with pu.Text() : pass
+      with pu.Shelf() :
+        with pu.Spacer() : pass
+        with pu.Grip() : pass
 
   def m_startup( self ) :
     sGeometry = pmq.request( 'm_geometry_get', 'editor' )
