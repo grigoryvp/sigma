0494a1a3dc54b96169c9cdc7b70dd9b5ba878a98
Grigory Petrov <grigory.v.p@gmail.com>
2012-06-30 01:03:18 +0400

    Added command-line options required for 'toc' functionality.

--- sigma_gui/commandline.py
+++ sigma_gui/commandline.py
@@ -8,10 +8,16 @@ import pmq
 class Commandline( pmq.Actor ) :
 
   def m_commandline_handle( self ) :
-    oParses = argparse.ArgumentParser( description = "Sigma" )
+    oParser = argparse.ArgumentParser( description = "Sigma" )
     sHelp = "Editor to interact"
-    lEditors = [ 'vim' ]
-    oParses.add_argument( '-editor', help = sHelp, choices = lEditors )
-    oArgs = oParses.parse_args()
+    lEditors = [ "vim" ]
+    oParser.add_argument( "-editor", help = sHelp, choices = lEditors )
+    sHelp = "File to process"
+    oParser.add_argument( "-file", help = sHelp )
+    oSubparsers = oParser.add_subparsers()
+    sHelp = "Display file table of content based on tags"
+    oSubparser = oSubparsers.add_parser( "-toc", help = sHelp )
+
+    oArgs = oParser.parse_args()
     pmq.post( 'm_wndeditor_show' )
 
