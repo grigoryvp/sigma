d17be78a7673a7a3dd1c03e0ec062251c77d966b
Grigory Petrov <grigory.v.p@gmail.com>
2012-06-29 20:25:47 +0400

    Added command-line args handling prototype.
    Not working yet - seems problem with "argparse" module internals.

--- sigma_gui/commandline.py
+++ sigma_gui/commandline.py
@@ -8,5 +8,18 @@ import pmq
 class Commandline( pmq.Actor ) :
 
   def m_commandline_handle( self ) :
-    pmq.post( 'm_wndeditor_show' )
+    try :
+      print( "1" )
+      oParses = argparse.ArgumentParser( description = "Sigma" )
+      print( "2" )
+      sHelp = "Editor to interact"
+      print( "3" )
+      lEditors = [ 'vim' ]
+      oParses.add_argument( '-editor', help = sHelp, choices = lEditors )
+      oArgs = oParses.parse_args()
+      print( "4" )
+      pmq.post( 'm_wndeditor_show' )
+      print( "5" )
+    except :
+      print( "exception" )
 
