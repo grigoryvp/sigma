5e0ed9311b182954699b9a6f12820389c0917c69
Grigory Petrov <grigory.v.p@gmail.com>
2012-08-14 21:29:45 +0400

    VIM bindings now works in "Projects" window.

--- sigma_gui/wnd_projects.py
+++ sigma_gui/wnd_projects.py
@@ -18,26 +18,29 @@ class WndProjects( WndEditorIntegrated ) :
         with pu.Label( name = 'info' ) :
           pu.o.setText( "Loading ..." )
           pu.o.alignCenter()
-        with pu.List( name = 'content' ) :
-          self.m_oItems = pu.o
+        with pu.List( name = 'content' ) : pass
       with pu.Shelf() :
         with pu.Spacer() : pass
         with pu.Grip() : pass
     self.setCaption( "Sigma: Projects" )
     self.bind( '<Return>', self.__onEnter )
 
+  def m_start( self ) :
+    self.o[ 'content' ].setKeys( pmq.request( 'm_cfg_get', 'keys' ) )
+
   def m_projects( self, i_lProjects ) :
     oCurrent = pmq.request( 'm_project_get' )
     for oProject in i_lProjects :
-      self.m_oItems.append( text = oProject.name, baton = oProject )
+      self.o[ 'content' ].append( text = oProject.name, baton = oProject )
       if oProject == oCurrent :
-        self.m_oItems.selectByBaton( oProject )
+        self.o[ 'content' ].selectByBaton( oProject )
     self.m_oStack.setCurrent( 'content' )
-    self.m_oItems.setFocus()
+    self.o[ 'content' ].setFocus()
 
   def __onEnter( self, i_oEvent ) :
-    lItems = self.m_oItems.selection()
+    lItems = self.o[ 'content' ].selection()
     if len( lItems ) :
-      pmq.post( 'm_project_set', self.m_oItems.idToBaton( lItems[ 0 ] ) )
+      sName = self.o[ 'content' ].idToBaton( lItems[ 0 ] )
+      pmq.post( 'm_project_set', sName )
       pmq.stop()
 
