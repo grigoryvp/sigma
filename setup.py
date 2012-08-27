7685b2b6c2b28b4a3505ccfafc8efb355d8f1d17
Grigory Petrov <grigory.v.p@gmail.com>
2012-08-27 13:27:08 +0400

    Refactoring so it can be used as Python package.

--- setup.py
+++ setup.py
@@ -2,15 +2,17 @@
 # -*- coding: utf-8 -*-
 
 from distutils.core import setup
+import setuptools
 
 setup(
-  name='sigma',
-  version='0.1.0',
-  description='Language neutral source code files preprocessor.',
-  author='Grigory Petrov',
-  author_email='grigory.v.p@gmail.com',
-  url='http://github.com/eyeofhell/sigma/',
-  packages=[ 'sigma' ],
+  name= "sigma",
+  version = '0.1.0',
+  description = "Language neutral source code files preprocessor.",
+  author = "Grigory Petrov",
+  author_email = "grigory.v.p@gmail.com",
+  url = "http://bitbucket.org/eyeofhell/sigma",
+  packages = [ 'sigma', 'sigma_gui' ],
+  scripts = [ 'sigma.py', 'sigma_gui.py' ],
   classifiers=[
     'Development Status :: 1 - Prototype',
     'Environment :: Console',
---
+++ sigma.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# sigma
+# Copyright 2011-2012 Grigory Petrov
+# See LICENSE for details.
+
+import sigma
+
