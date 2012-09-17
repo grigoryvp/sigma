6d76b28fe4eae9b9c99acd4bbe35e7a16bdba859
Grigory Petrov <grigory.v.p@gmail.com>
2012-09-17 21:31:49 +0400

    Added round robin selector to project scanner.

--- sigma_gui_impl/project_status_scan.py
+++ sigma_gui_impl/project_status_scan.py
@@ -9,6 +9,7 @@ class ProjectStatusScan( pmq.Actor ) :
   def __init__( self ) :
     pmq.Actor.__init__( self )
     self.m_lProjects = []
+    self.m_nCurrentProject = 0
 
   def m_startup( self ) :
     pmq.post( 'm_project_status_scan', delay = 1.0 )
@@ -17,5 +18,17 @@ class ProjectStatusScan( pmq.Actor ) :
     self.m_lProjects = i_lProjects
 
   def m_project_status_scan( self ) :
+    oProject = self.__nextProject()
+    if oProject is not None :
+      print( "Processing {0}".format( oProject.name ) )
     pmq.post( 'm_project_status_scan', delay = 1.0 )
+
+  ##  Round robin next project pickup.
+  def __nextProject( self ) :
+    self.m_nCurrentProject += 1
+    if self.m_nCurrentProject >= len( self.m_lProjects ) :
+      self.m_nCurrentProject = 0
+    if self.m_nCurrentProject < len( self.m_lProjects ) :
+      return self.m_lProjects[ self.m_nCurrentProject ]
+    return None
 
