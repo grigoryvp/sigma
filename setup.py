b16e967cb3c79acc1b8c9953d3e83277a476c490
Grigory Petrov <grigory.v.p@gmail.com>
2012-11-28 14:42:55 +0400

    Refactoring complete. Installation is now a little better.

--- setup.py
+++ setup.py
@@ -1,24 +1,31 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from distutils.core import setup
 import setuptools
+import sigma.info
 
-setup(
-  name= "sigma",
-  version = '0.1.0',
-  description = "Language neutral source code files preprocessor.",
-  author = "Grigory Petrov",
+setuptools.setup(
+  name         = sigma.info.NAME_SHORT,
+  version      = sigma.info.VER_TXT,
+  description  = "Language neutral source code files preprocessor.",
+  author       = "Grigory Petrov",
   author_email = "grigory.v.p@gmail.com",
-  url = "http://bitbucket.org/eyeofhell/sigma",
-  packages = [ 'sigma_impl', 'sigma_gui_impl' ],
-  scripts = [ 'sigma.py', 'sigma_gui.pyw' ],
-  classifiers=[
+  url          = "http://bitbucket.org/eyeofhell/sigma",
+  packages     = [ 'sigma', 'sigma_gui' ],
+  entry_points = {
+    'console_scripts' : [
+      'sigma = sigma:main',
+      'sigmag = sigma_gui:main',
+    ]
+  },
+  classifiers  = [
     'Development Status :: 1 - Prototype',
     'Environment :: Console',
     'Intended Audience :: Developers',
     'License :: GPLv3',
     'Operating System :: OS Independent',
     'Programming Language :: Python',
-    'Topic :: Utilities' ])
+    'Topic :: Utilities'
+  ],
+)
 
